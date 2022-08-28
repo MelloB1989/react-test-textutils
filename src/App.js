afa53a4a4930d194e747be496e9c66a691d7c54f
Kartik <kartik@mellob.co>
2022-08-28 11:55:57 +0530

    Fixed Bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
-import './index.css'
+import './index.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
 import { useState } from 'react';
+import React from 'react';
 import Alert from './components/Alert';
 import About from './components/About';
 
 function App() {
 
   const [mode, setMode] = useState('light');
-  const [btntxt, setBtnText] = useState('Dark Mode')
+  const [btntxt, setBtnText] = useState('Dark Mode');
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type)=>{
     setAlert({
       message: message,
       type: type
-    })
+    });
 
     setTimeout(()=>{
       setAlert(null);
@@ -26,17 +27,16 @@ function App() {
 
   const toggleMode = ()=>{
     if (mode === 'light'){
-      setMode('dark')
+      setMode('dark');
       document.body.style.backgroundColor = '#212529';
-      Navbar.border.style.color = 'white'
-      setBtnText('Light Mode')
+      setBtnText('Light Mode');
     }
     else {
-      setMode('light')
+      setMode('light');
       document.body.style.backgroundColor = 'white';
-      setBtnText('Dark Mode')
+      setBtnText('Dark Mode');
     }
-  }
+  };
 
   return (
     <>
